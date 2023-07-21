839e13143361d52f98c207502a75b9e8101aca8f
THUOC LE <anhtuan1990nxht@gmail.com>
2023-07-21 17:52:38 +0700

    update chude API-Interface-Services

---
+++ LTS-EDU-FINAL/Controllers/ChuDeController.cs
@@ -0,0 +1,57 @@
+﻿using LTS_EDU_FINAL.Constant;
+using LTS_EDU_FINAL.Entities;
+using LTS_EDU_FINAL.IServices;
+using LTS_EDU_FINAL.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LTS_EDU_FINAL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChuDeController : ControllerBase
+    {
+        private readonly IChuDe _ChuDeServices;
+        public ChuDeController()
+        {
+            _ChuDeServices = new ChuDeServices();
+        }
+        [HttpPost("themChuDe")]
+        public async Task<IActionResult> ThemChuDe([FromBody] ChuDe cd)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("DuLieuNhapVaoKhongDu");
+            var ret = await _ChuDeServices.ThemChuDeAsync(cd);
+            if (ret == ErrorMessage.ThanhCong)
+                return Ok("Them thanh cong");
+            if (ret == ErrorMessage.DaTonTai)
+                return BadRequest("Chu de đã tồn tại");
+            return BadRequest("Them That bai");
+        }
+        [HttpPut("suaChuDe")]
+        public async Task<IActionResult> SuaChuDe([FromBody] ChuDe cd, [FromQuery] int cdID)
+        {
+            var ret = await _ChuDeServices.SuaChuDeAsync(cd, cdID);
+            if (ret == ErrorMessage.ThanhCong)
+                return Ok("Sua thanh cong");
+            if (ret == ErrorMessage.KhongTonTai)
+                return BadRequest("Chu de khong tồn tại");
+            return BadRequest("Sua That bai");
+        }
+        [HttpDelete("xoaChuDe")]
+        public async Task<IActionResult> XoaChuDe([FromQuery] int cdID)
+        {
+            var ret = await _ChuDeServices.XoaChuDeAsync(cdID);
+            if (ret == ErrorMessage.ThanhCong)
+                return Ok("Xoa thanh cong");
+            if (ret == ErrorMessage.KhongTonTai)
+                return BadRequest("Chu de khong tồn tại");
+            return BadRequest("Xoa That bai");
+        }
+        [HttpGet("hienThiChuDe")]
+        public async Task<IActionResult> HienThiChuDe([FromQuery] Pagination page)
+        {
+            return Ok(await _ChuDeServices.HienThiChuDeAsync(page));
+        }
+    }
+}
--- LTS-EDU-FINAL/Services/ChuDeServices.cs
+++ LTS-EDU-FINAL/Services/ChuDeServices.cs
@@ -1,6 +1,114 @@
-﻿namespace LTS_EDU_FINAL.Services
+﻿using AutoMapper;
+using LTS_EDU_FINAL.Constant;
+using LTS_EDU_FINAL.Context;
+using LTS_EDU_FINAL.Entities;
+using LTS_EDU_FINAL.IServices;
+using Microsoft.EntityFrameworkCore;
+
+namespace LTS_EDU_FINAL.Services
 {
-    public class ChuDeServices
+    public class ChuDeServices : IChuDe
     {
+        private readonly AppDbContext dbContext;
+
+        public ChuDeServices()
+        {
+            this.dbContext = new AppDbContext();
+        }
+        #region Private 
+        private async Task<ChuDe> GetChuDe(int cdID)
+        {
+            return await dbContext.ChuDe.FirstOrDefaultAsync(x => x.ChuDeID == cdID);
+        }
+        private async Task<bool> TenChuDeExistenceAsync(string tencd)
+        {
+            return await dbContext.ChuDe.AnyAsync(x => x.TenChuDe == tencd);
+        }
+        #endregion
+        public Task<PageInfo<ChuDe>> HienThiChuDeAsync(Pagination page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ErrorMessage> SuaChuDeAsync(ChuDe cd, int cdID)
+        {
+            using (var trans = await dbContext.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    //tim ra  de sua
+                    var cdNow = await GetChuDe(cdID);
+                    if (cdNow == null)
+                        return ErrorMessage.KhongTonTai;
+                    var config = new MapperConfiguration(cfg => {
+                        cfg.CreateMap<ChuDe, ChuDe>()
+                         .ForMember(dest => dest.ChuDeID, opt => opt.Ignore());
+                    });
+                    var mapper = new Mapper(config);
+                    // Ánh xạ thông tin từ cd vào Now
+                    mapper.Map(cd, cdNow);
+                    dbContext.Update(cdNow);
+                    await dbContext.SaveChangesAsync();
+                    // Commit transaction
+                    await trans.CommitAsync();
+                    return ErrorMessage.ThanhCong;
+                }
+                catch (Exception)
+                {
+                    // Nếu có lỗi xảy ra, rollback transaction và ném ra ngoại lệ
+                    await trans.RollbackAsync();
+                    throw;
+                }
+            }
+        }
+
+        public async Task<ErrorMessage> ThemChuDeAsync(ChuDe cd)
+        {
+            using (var trans = await dbContext.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    if (await TenChuDeExistenceAsync(cd.TenChuDe))
+                        return ErrorMessage.DaTonTai;
+                    await dbContext.AddAsync(cd);
+                    await dbContext.SaveChangesAsync();
+                    // Commit transaction
+                    await trans.CommitAsync();
+                    return ErrorMessage.ThanhCong;
+                }
+                catch (Exception)
+                {
+                    // Nếu có lỗi xảy ra, rollback transaction và ném ra ngoại lệ
+                    await trans.RollbackAsync();
+                    throw;
+                }
+            }
+        }
+
+        public async Task<ErrorMessage> XoaChuDeAsync(int cdID)
+        {
+            using (var trans = await dbContext.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    //tim ra  de sua
+                    var cdNow = await GetChuDe(cdID);
+                    if (cdNow == null)
+                        return ErrorMessage.KhongTonTai;
+
+                    dbContext.Remove(cdNow);
+                    await dbContext.SaveChangesAsync();
+                    // Commit transaction
+                    await trans.CommitAsync();
+                    return ErrorMessage.ThanhCong;
+                }
+                catch (Exception)
+                {
+                    // Nếu có lỗi xảy ra, rollback transaction và ném ra ngoại lệ
+                    await trans.RollbackAsync();
+                    throw;
+                }
+            }
+        }
     }
 }
